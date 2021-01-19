56026646290d80f0a9fae31407b47b07fd961cb0
Maria Garcia de Lomana <maria@zenseiapp.com>
2021-01-19 08:54:56 +0100

    feat: connect SignIn to api

---
+++ src/api/loginUser.ts
@@ -0,0 +1,32 @@
+import axios from "axios";
+import {User, UserAPIResponse} from "../types";
+import {BASE_URL, BASE_POST_USER_DATA} from "../host_backend";
+
+async function loginUser (user: Omit<User, "id"| "first_name"| "last_name">): Promise<UserAPIResponse| undefined> {
+  try {
+    const response = await axios({
+      method: "post",
+      url: `${BASE_URL}/users/login`,
+      data: {...BASE_POST_USER_DATA, ...user }
+    });
+    if (response?.status === 200) {
+      return {
+        success: true,
+        data: response.data,
+      };
+    } else if (response?.status === 401) {
+      return {
+        success: false,
+        error: "There is no account registered with this data."
+      };
+    } else {
+      return {
+        success: false,
+        error: "An error has occurred. Please, try again later."
+      };
+    }
+  } catch (error) {
+    console.error("Unable to login user: ", error);
+  }
+}
+export default loginUser;
