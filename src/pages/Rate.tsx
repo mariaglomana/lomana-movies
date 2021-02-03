d89b612faeee639184ac3062f4147ee0358aa449
Maria Garcia de Lomana <maria@zenseiapp.com>
2021-02-03 21:09:15 +0100

    style: improve responsive layout of Rate page

--- src/pages/Rate.tsx
+++ src/pages/Rate.tsx
@@ -1,13 +1,29 @@
 import React, { useState, useEffect, useCallback } from "react";
 import { useHistory } from "react-router-dom";
+import clsx from "clsx";
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
 
 import { MoviePreviewData } from "types";
 import { PageContainer, ImageGridItem } from "components";
 import { ActionRateGroupButtons } from "layout";
 import { getUnratedRandomMovie, registerRate } from "api";
 
+const useStyles = makeStyles((theme) => ({
+  center: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  column: {
+    minWidth: 176,
+    padding: theme.spacing(2),
+  },
+}));
+
 const Rate: React.FC = () => {
   const history = useHistory();
+  const classes = useStyles();
 
   const [unratedMovie, setUnratedMovie] = useState<
     MoviePreviewData | undefined
@@ -46,14 +62,24 @@ const Rate: React.FC = () => {
 
   return (
     <PageContainer title="Rate random movies">
-      <ImageGridItem item={unratedMovie} />
-      <ActionRateGroupButtons
-        rate={rate}
-        loadRandomMovie={memoizedLoadRandomMovie}
-        setRate={setRate}
-        handleSaveRate={handleSaveRate}
-        handleClickStar={handleClickStar}
-      />
+      <Grid container direction="row" className={classes.center} spacing={3}>
+        <Grid item className={classes.column}>
+          <ImageGridItem item={unratedMovie} />
+        </Grid>
+        <Grid
+          item
+          xs={10}
+          md={3}
+          className={clsx(classes.column, classes.center)}>
+          <ActionRateGroupButtons
+            rate={rate}
+            loadRandomMovie={memoizedLoadRandomMovie}
+            setRate={setRate}
+            handleSaveRate={handleSaveRate}
+            handleClickStar={handleClickStar}
+          />
+        </Grid>
+      </Grid>
     </PageContainer>
   );
 };
